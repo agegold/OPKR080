cf8003741dc30bd0963237a5d2a237b9131a8a31
chanhojung <jungch@naver.com>
2020-12-08 09:42:21 +0900

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -151,11 +151,18 @@ class CarState(CarStateBase):
 
     self.cruiseGapSet = cp_scc.vl["SCC11"]['TauGapSet']
 
+    print(cp.vl["CLU15"])
+    print(cp.vl["TCU12"])
+    print(cp.vl["ELECT_GEAR"])
+    print(cp.vl["LVR12"])
 
     # TODO: refactor gear parsing in function
     # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection,
     # as this seems to be standard over all cars, but is not the preferred method.
     if self.CP.carFingerprint in FEATURES["use_cluster_gears"]:
+      gear_disp = cp.vl["CLU15"]
+      print(gear_disp)
+      print(gear)      
       if cp.vl["CLU15"]["CF_Clu_InhibitD"] == 1:
         ret.gearShifter = GearShifter.drive
       elif cp.vl["CLU15"]["CF_Clu_InhibitN"] == 1:
